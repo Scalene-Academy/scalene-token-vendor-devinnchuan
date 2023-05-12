e9297f3c8005f9caf94eecd428cd172dfc889427
Devinn Chuan <devinnchuan@gmail.com>
2023-05-12 21:00:29 +1000

    Finished

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -3,10 +3,42 @@ pragma solidity 0.8.19; //Do not change the solidity version as it negativly imp
 
 // learn more: https://docs.openzeppelin.com/contracts/4.x/api/access#Ownable
 // @TODO: Vendor should inherit Ownable
-// import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./YourToken.sol";
 
 // @TODO: Implement the Vendor smart contract
-contract Vendor {
+contract Vendor is Ownable{
+    uint256 public constant tokensPerEth = 100;
+
+    YourToken public yourToken; // Instance of YourToken contract
+
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress); // Initialize the YourToken instance
+    }
+
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+
+    function buyTokens() public payable{
+        uint256 amount = msg.value * tokensPerEth;
+        yourToken.transfer(msg.sender, amount);
+        emit BuyTokens(msg.sender, msg.value, amount);
+    }
+
+    //Withdraw all ETH from contract to owner address
+    function withdraw() public onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    function sellTokens(uint256 amount) public {
+
+    uint256 ethAmount = amount / tokensPerEth;
+
+    // Transfer tokens from the user to the contract
+    yourToken.transferFrom(msg.sender, address(this), amount);
+
+    // Transfer ETH from the contract to the user
+    payable(msg.sender).transfer(ethAmount);
+    }
 
 }
